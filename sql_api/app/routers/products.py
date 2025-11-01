fe55b6581f822e5931638f058ec71c6b910537da
InshutiSouvede <isouvede2@gmail.com>
2025-11-01 15:09:20 +0200

    feat: products crud

---
+++ sql_api/app/routers/products.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app import controllers, schemas, database
+
+router = APIRouter(prefix="/products", tags=["Products"])
+
+@router.get("/", response_model=list[schemas.Product])
+def list_products(db: Session = Depends(database.get_db)):
+    return controllers.products.get_products(db)
+
+@router.get("/{product_id}", response_model=schemas.Product)
+def read_product(product_id: int, db: Session = Depends(database.get_db)):
+    db_product = controllers.products.get_product(db, product_id)
+    if not db_product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return db_product
+
+@router.post("/", response_model=schemas.Product)
+def create_product(product: schemas.ProductCreate, db: Session = Depends(database.get_db)):
+    return controllers.products.create_product(db, product)
+
+@router.put("/{product_id}", response_model=schemas.Product)
+def update_product(product_id: int, product: schemas.ProductCreate, db: Session = Depends(database.get_db)):
+    return controllers.products.update_product(db, product_id, product)
+
+@router.delete("/{product_id}")
+def delete_product(product_id: int, db: Session = Depends(database.get_db)):
+    return controllers.products.delete_product(db, product_id)
