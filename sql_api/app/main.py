c71a70e599e81ee0c324afcae525a0e1a92bd789
Souvede Inshuti <77120594+InshutiSouvede@users.noreply.github.com>
2025-11-02 13:28:11 +0200

    Import models in main.py for database setup

--- sql_api/app/main.py
+++ sql_api/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.database import Base, engine
+from app import models
 from app.routers import customers, products, orders, order_items, reviews
 
 Base.metadata.create_all(bind=engine)
